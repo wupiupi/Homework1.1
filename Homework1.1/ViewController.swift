73498b5aa382c960f41c81602b433b7d3c2651d6
Jormungandrp <90474212+Jormungandrp@users.noreply.github.com>
2022-02-12 17:06:00 +0300

    Initial Commit

---
+++ Homework1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework1.1
+//
+//  Created by Paul on 12.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
